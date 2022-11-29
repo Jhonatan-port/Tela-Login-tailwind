43fb78faf3c87d5bbfe5fb7601a7bb8f8db4fcac
Jhonatan dos Santos <36736435+Jhonatan-port@users.noreply.github.com>
2022-11-29 20:57:23 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      borderRadius: {
+        '4xl': '5rem',
+        
+      }
+    },
+  },
+  plugins: [],
+}
